09fd87954526510527a5a484b3829b2fb934ca29
Fizz Nomad <128290319+FizzNomad@users.noreply.github.com>
2023-10-24 18:05:57 +0000

    Create vpnbash.sh
    VPN Monitor for HTB. added to a lower gui panel

---
+++ vpnbash.sh
@@ -0,0 +1,11 @@
+## VPN Monitor for HTB. Add this file to /opt/
+
+#!/bin/bash
+htbip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+
+if [[ $htbip == *"10."* ]]
+then
+   echo "$htbip"
+else
+   echo ""
+fi
